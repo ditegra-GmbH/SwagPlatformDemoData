1f10cfa101f2b5c4a8ee3e241b9da974ef727ef1
Sergej <sergej.riewe@ditegra.de>
2023-12-13 20:19:56 +0100

    add product stream provider to create dynamic product group for qpc tests

--- src/DataProvider/ProductStreamProvider.php
+++ src/DataProvider/ProductStreamProvider.php
@@ -3,22 +3,15 @@
 namespace Swag\PlatformDemoData\DataProvider;
 
 use Doctrine\DBAL\Connection;
-use Shopware\Core\Framework\Log\Package;
 use Swag\PlatformDemoData\Resources\helper\TranslationHelper;
 
-
-#[Package('services-settings')]
 class ProductStreamProvider extends DemoDataProvider
 {
-   
     private Connection $connection;
-
-    private TranslationHelper $translationHelper;
-
+    
     public function __construct(Connection $connection)
     {
         $this->connection = $connection;
-        $this->translationHelper = new TranslationHelper($connection);
     }
 
     public function getAction(): string
@@ -35,7 +28,7 @@ class ProductStreamProvider extends DemoDataProvider
     {
         return [
             [
-                'id' => "dc6ea235320c4f40aeb28dcc16496a28",
+                'id' => 'dc6ea235320c4f40aeb28dcc16496a28',
                 'name' => 'TST100 - QPC10001',
                 'filters' => [
                     [
@@ -43,20 +36,16 @@ class ProductStreamProvider extends DemoDataProvider
                         'operator' => 'OR',
                         'queries' => [
                             [
-                                'queries' => [
+                                'type'=> 'multi',
+                                'operator'=> 'AND',
+                                'position'=> 0,
+                                'queries'=> [
                                     [
-                                        'type' => 'equals',
-                                        'field' => 'product.id',
-                                        'value' => '2a88d9b59d474c7e869d8071649be43c',
-                                    ],
-                                ],
-                                'queries' => [
-                                    [
-                                        'type' => 'equals',
-                                        'field' => 'product.parentId',
-                                        'value' => '2a88d9b59d474c7e869d8071649be43c',
-                                    ],
-                                ],                                
+                                        'type'=> 'equals',
+                                        'field'=> 'id',
+                                        'value'=> '2a88d9b59d474c7e869d8071649be43c'
+                                    ]
+                                ]
                             ],
                         ],
                     ],
