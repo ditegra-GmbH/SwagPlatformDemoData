5b32f0ef3a13e90b98207fd9d08eb2a286cd0994
arthur-ditegra <arthur.erlich@ditegra.de>
2023-12-20 14:24:16 +0100

    added AiMediaProvider class

---
+++ src/AiDataProvider/AiMediaProvider.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+/*
+ * (c) shopware AG <info@shopware.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swag\PlatformDemoData\DataProvider;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Content\Media\File\FileSaver;
+use Shopware\Core\Content\Media\File\MediaFile;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Log\Package;
+
+#[Package('services-settings')]
+class AiMediaProvider extends MediaProvider{
+    
+}
